1ac5db4794b1ed7a167ed480ba4d629b90011bd2
Wayne.Zou <Wayne.Zou@welocalize.com>
2015-06-01 10:57:27 +0800

    (GBS-3954) Support InDesign CC for In Context Review

---
+++ main6/diplomat/dev/src/c-sharp/AdobeConverter_CC2014/AdobeConverter/AdobeConverter/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using GlobalSight.Common;
+using System.Threading;
+
+namespace GlobalSight.AdobeConverter
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainForm mf = new MainForm();
+
+            if (AppConfig.AutoStart)
+            {
+                Thread t = new Thread(new ThreadStart(mf.autoStart));
+                t.Start();
+            }
+
+            Application.Run(mf);
+        }
+    }
+}

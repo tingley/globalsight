cfa18fb6b123e85a985e63e55e1a22c50235feb3
YorkJin <YorkJin@YorkJin-Win7-PC.welocalize.com>
2015-09-08 12:32:08 +0800

    GBS-3676 & GBS-3650: remove abandoned tables.

--- main6/tools/install/data/ERs/8.6.4.0/GlobalSight_8.6.4.0.4.sql
+++ main6/tools/install/data/ERs/8.6.4.0/GlobalSight_8.6.4.0.4.sql
@@ -2,7 +2,46 @@
 # GBS-3676: Record job name at TUV level in Translation Memory.
 
 # drop all "tm3_tu_tuv_attr_shared_xx" tables
-SET @v = (SELECT CONCAT('drop table ', GROUP_CONCAT(a.table_name)) FROM information_schema.tables a WHERE a.table_schema = DATABASE() AND a.table_name LIKE 'tm3_tu_tuv_attr_shared_%' );
-SET @Y = (SELECT IF (@v IS NOT NULL, @v, 'select 1'));
-PREPARE s FROM @Y;
-EXECUTE s;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS dropTm3AttrTables$$
+
+CREATE PROCEDURE dropTm3AttrTables()
+BEGIN
+	DECLARE done INTEGER DEFAULT 0;
+	DECLARE tm3AttrTable VARCHAR(60);
+
+	-- cursor
+	DECLARE tm3_attr_table_cur CURSOR FOR 
+		SELECT table_name FROM information_schema.TABLES 
+		WHERE TABLE_SCHEMA = DATABASE() 
+		AND table_name LIKE 'tm3_tu_tuv_attr_shared_%';
+
+	-- error handler 
+	DECLARE EXIT HANDLER FOR SQLSTATE '02000' SET done = 1;
+	DECLARE CONTINUE HANDLER FOR SQLSTATE '42S02' BEGIN END;
+
+	OPEN tm3_attr_table_cur;
+	tm3AttrTableName_lable: LOOP
+		FETCH tm3_attr_table_cur INTO tm3AttrTable;
+		# logger
+		SELECT tm3AttrTable AS CURRENT_TM3_ATTR_TABLE_TO_DELETE;
+
+		IF done = 1 THEN
+			LEAVE tm3AttrTableName_lable;
+		END IF;
+
+		SET @sql1 = CONCAT("DROP TABLE IF EXISTS ", tm3AttrTable);
+		PREPARE stmt1 FROM @sql1;
+		EXECUTE stmt1;
+		DEALLOCATE PREPARE stmt1;
+
+	END LOOP;
+	CLOSE tm3_attr_table_cur;
+    END$$
+
+DELIMITER ;
+
+
+CALL dropTm3AttrTables;
+DROP PROCEDURE IF EXISTS dropTm3AttrTables;

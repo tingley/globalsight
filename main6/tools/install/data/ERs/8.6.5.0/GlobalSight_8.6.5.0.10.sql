d8555e3f48ce8bd7de3e48b71996d9c69e33c975
Belinda.zhang <Belinda.zhang@welocalize>
2015-10-13 16:37:09 +0800

    GBS-4026:Newly added available categories are lost.

---
+++ main6/tools/install/data/ERs/8.6.5.0/GlobalSight_8.6.5.0.10.sql
@@ -0,0 +1,10 @@
+# GBS-4026 : Newly added available categories are lost
+
+RENAME TABLE category TO category_segement_comment;
+ALTER TABLE category_segement_comment ADD COLUMN IS_ACTIVE VARCHAR(1) NOT NULL DEFAULT 'Y';
+
+RENAME TABLE post_review_category TO category_post_review;
+ALTER TABLE category_post_review ADD COLUMN IS_ACTIVE VARCHAR(1) NOT NULL DEFAULT 'Y';
+
+RENAME TABLE scorecard_category TO category_scorecard;
+ALTER TABLE category_scorecard ADD COLUMN IS_ACTIVE VARCHAR(1) NOT NULL DEFAULT 'Y';

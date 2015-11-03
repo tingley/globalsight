1334374241d72711094c5aa92d87a3864ba1e025
Wayne.Zou <Wayne.Zou@welocalize.com>
2015-11-03 15:01:03 +0800

    GBS-4039_defaultSRX - support default rule before

--- main6/envoy/src/java/com/globalsight/everest/segmentationhelper/SegmentationHelper.java
+++ main6/envoy/src/java/com/globalsight/everest/segmentationhelper/SegmentationHelper.java
@@ -16,11 +16,16 @@
  */
 package com.globalsight.everest.segmentationhelper;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
+
+import com.globalsight.ling.common.XmlEntities;
+import com.globalsight.ling.docproc.Segmenter;
 
 public class SegmentationHelper
 {
-
     /**
      * This method is used to segment text accoding to segmentation rule text,
      * and return an String[] filled with segments after segmentation
@@ -52,4 +57,80 @@ public class SegmentationHelper
         return newSegments;
     }
 
+    public static String[] segmentWithDefault(Locale p_locale, String p_text)
+            throws Exception
+    {
+        SegmentationHelper shelper = new SegmentationHelper();
+        Segmenter m_defaultSegmenter = new Segmenter(p_locale);
+        
+        m_defaultSegmenter.setText(p_text);
+        
+        ArrayList breakPositions = new ArrayList();
+        
+        int iStart = m_defaultSegmenter.first();
+        for (int iEnd = m_defaultSegmenter.next(); iEnd != Segmenter.DONE; iStart = iEnd, iEnd = m_defaultSegmenter
+                .next())
+        {
+            // System.err.println("--> `" +
+            // m_segmentWithoutTags.substring(iStart, iEnd) + "'");
+
+            BreakPosition pos = shelper.new BreakPosition(iEnd);
+            breakPositions.add(pos);
+        }
+        
+        // split segments
+        return splitOriSegment(breakPositions, p_text);
+    }
+    
+    private static String[] splitOriSegment(ArrayList p_breaks, String p_text)
+    {
+        List<String> result = new ArrayList<String>();
+
+        // offsets in m_segment
+        int iStart = 0;
+        int iEnd;
+
+        // If the section was empty, restore the original input.
+        if (p_breaks.size() == 0)
+        {
+            result.add(p_text);
+        }
+
+        else
+        {
+            // Else compute the segment breaks in the original input and
+            // add the segments to the segment list.
+            for (Iterator it = p_breaks.iterator(); it.hasNext();)
+            {
+                BreakPosition pos = (BreakPosition) it.next();
+
+                iEnd = pos.m_split;
+
+                if (iEnd > p_text.length())
+                {
+                    result.add(p_text.substring(iStart));
+                }
+                else
+                {
+                    result.add(p_text.substring(iStart, iEnd));
+                }
+
+                iStart = iEnd;
+            }
+        }
+
+        String[] strs = new String[result.size()];
+        return result.toArray(strs);
+    }
+    
+    final private class BreakPosition
+    {
+        // Offset of segment boundary
+        public int m_split = 0;
+
+        public BreakPosition(int p_pos)
+        {
+            m_split = p_pos;
+        }
+    }
 }

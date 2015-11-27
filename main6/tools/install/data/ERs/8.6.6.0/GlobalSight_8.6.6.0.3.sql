5ab3b93dac798a5b665cd8b563768e1f8f23affa
allie.su <allie.su@welocalize.com>
2015-11-27 14:51:11 +0800

    0001395: GBS-3885: Upgrade error caused by table innodb_sys_indexes in GlobalSight_8.6.6.0.3.sql

--- main6/tools/install/data/ERs/8.6.6.0/GlobalSight_8.6.6.0.3.sql
+++ main6/tools/install/data/ERs/8.6.6.0/GlobalSight_8.6.6.0.3.sql
@@ -7,50 +7,59 @@ DROP PROCEDURE IF EXISTS createIdxFor3885$$
 
 CREATE PROCEDURE createIdxFor3885()
 BEGIN
-	DECLARE done INTEGER DEFAULT 0;
-	DECLARE tm3TuvTable VARCHAR(60);
+ DECLARE done INTEGER DEFAULT 0;
+ DECLARE tm3TuvTable VARCHAR(60);
 
-	-- cursor
-	DECLARE tm3_tuv_shared_table_cur CURSOR FOR 
-		SELECT table_name FROM information_schema.TABLES 
-		WHERE TABLE_SCHEMA = DATABASE() 
-		AND table_name LIKE 'tm3_tuv_shared_%';
+ -- cursor
+ DECLARE tm3_tuv_shared_table_cur CURSOR FOR 
+  SELECT table_name FROM information_schema.TABLES 
+  WHERE TABLE_SCHEMA = DATABASE() 
+  AND table_name LIKE 'tm3_tuv_shared_%';
 
-	-- error handler 
-	DECLARE EXIT HANDLER FOR SQLSTATE '02000' SET done = 1;
+ -- error handler 
+ DECLARE EXIT HANDLER FOR SQLSTATE '02000' SET done = 1;
 
-	OPEN tm3_tuv_shared_table_cur;
-	tm3TuvTableName_lable: LOOP
-		FETCH tm3_tuv_shared_table_cur INTO tm3TuvTable;
-		# logger
-		SELECT tm3TuvTable AS CURRENT_TM3_TUV_TABLE_NAME;
+ OPEN tm3_tuv_shared_table_cur;
+ tm3TuvTableName_lable: LOOP
+  FETCH tm3_tuv_shared_table_cur INTO tm3TuvTable;
+  # logger
+  SELECT tm3TuvTable AS CURRENT_TM3_TUV_TABLE_NAME;
 
-		IF done = 1 THEN
-			LEAVE tm3TuvTableName_lable;
-		END IF;
-
-		SET @sql1 = CONCAT(
+  IF done = 1 THEN
+   LEAVE tm3TuvTableName_lable;
+  END IF;
+  
+  SET @checkTableExistSql = CONCAT("SELECT COUNT(*) INTO @tableNum FROM information_schema.TABLES WHERE TABLE_NAME = 'innodb_sys_indexes'");
+  PREPARE stmt0 FROM @checkTableExistSql;
+  EXECUTE stmt0;
+  DEALLOCATE PREPARE stmt0;
+  # logger
+  SELECT @tableNum AS TABLE_IS_EXIST;
+  
+  IF @tableNum = 0 THEN
+	SET @checkIndexSql = CONCAT("SELECT COUNT(*) INTO @indexNum FROM information_schema.KEY_COLUMN_USAGE AS kcu WHERE kcu.TABLE_SCHEMA = '",
+			DATABASE(),"'"," AND kcu.CONSTRAINT_NAME ='INDEX_LOCALE_ID' AND kcu.table_name =","'",tm3TuvTable,"'");
+  ELSE
+	SET @checkIndexSql = CONCAT(
 			"SELECT COUNT(*) INTO @indexNum FROM information_schema.innodb_sys_indexes AS idx, ",
 			"(SELECT table_id FROM information_schema.innodb_sys_tables WHERE NAME LIKE ", "'", DATABASE(),"/", tm3TuvTable, "') AS tableId ",
 			"WHERE idx.table_id = tableId.table_id AND idx.name = 'INDEX_LOCALE_ID'");
-		# logger
-		# select @sql1 as SQL1;
-		
-		PREPARE stmt1 FROM @sql1;
-		EXECUTE stmt1;
-		DEALLOCATE PREPARE stmt1;
-		# logger
-		SELECT @indexNum AS INDEX_IS_CREATED;
+  PREPARE stmt1 FROM @checkIndexSql;
+  EXECUTE stmt1;
+  DEALLOCATE PREPARE stmt1;
+  END IF;
+  # logger
+  SELECT @indexNum AS INDEX_IS_CREATED;
 
-		IF @indexNum = 0 THEN
-			SET @createIdx = CONCAT("ALTER TABLE ", tm3TuvTable, " ADD INDEX INDEX_LOCALE_ID(localeId)");
-			PREPARE stmt2 FROM @createIdx;
-			EXECUTE stmt2;
-			DEALLOCATE PREPARE stmt2;
-		END IF;
+  IF @indexNum = 0 THEN
+	SET @createIdx = CONCAT("ALTER TABLE ", tm3TuvTable, " ADD INDEX INDEX_LOCALE_ID(localeId)");
+   PREPARE stmt2 FROM @createIdx;
+   EXECUTE stmt2;
+   DEALLOCATE PREPARE stmt2;
+  END IF;
 
-	END LOOP;
-	CLOSE tm3_tuv_shared_table_cur;
+ END LOOP;
+ CLOSE tm3_tuv_shared_table_cur;
     END$$
 
 DELIMITER ;

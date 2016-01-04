da59de6b3f15905ade6ad1206d013b0e467719fc
Belinda.zhang <Belinda.zhang@welocalize>
2016-01-04 16:01:44 +0800

    GBS-4156:Add "signed-off" as a target string status that will allow a translated string to be over-written by complete English string.

--- main6/tools/client tools/GlobalSight Installer/buildTool_CVS/script/windows/unzip.bat
+++ main6/tools/client tools/GlobalSight Installer/buildTool_CVS/script/windows/unzip.bat
@@ -1,2 +1,2 @@
-cd /D "D:\\GlobalSight_Master/main6/tools/build/dist"
-jar -xfv GlobalSight_20151109.zip
+cd /D "F:\\code/GlobalSight_Master/main6/tools/build/dist"
+jar -xfv GlobalSight_20160104.zip

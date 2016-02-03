72db4f56388f4b438aa53a34ad6b6c3b673f5e82
YorkJin <YorkJin@YorkJin-Win7-PC.welocalize.com>
2016-02-03 09:28:40 +0800

    code style format.

--- main6/envoy/src/java/com/globalsight/everest/webapp/pagehandler/administration/config/xmlrulefile/XmlRuleFileMainHandler.java
+++ main6/envoy/src/java/com/globalsight/everest/webapp/pagehandler/administration/config/xmlrulefile/XmlRuleFileMainHandler.java
@@ -49,24 +49,26 @@ import com.globalsight.util.StringUtil;
 import com.globalsight.util.edit.EditUtil;
 
 /**
- * XmlRuleFilePageHandler, A page handler to produce the entry page
- * (index.jsp) for XmlRuleFile management.
+ * XmlRuleFilePageHandler, A page handler to produce the entry page (index.jsp)
+ * for XmlRuleFile management.
  */
-public class XmlRuleFileMainHandler
-    extends PageHandler
+public class XmlRuleFileMainHandler extends PageHandler
 {
     /**
      * Invokes this PageHandler
      *
-     * @param p_pageDescriptor the page desciptor
-     * @param p_request the original request sent from the browser
-     * @param p_response the original response object
-     * @param p_context context the Servlet context
+     * @param p_pageDescriptor
+     *            the page desciptor
+     * @param p_request
+     *            the original request sent from the browser
+     * @param p_response
+     *            the original response object
+     * @param p_context
+     *            context the Servlet context
      */
-    public void invokePageHandler(WebPageDescriptor p_pageDescriptor,
-        HttpServletRequest p_request, HttpServletResponse p_response,
-        ServletContext p_context)
-        throws ServletException, IOException, EnvoyServletException
+    public void invokePageHandler(WebPageDescriptor p_pageDescriptor, HttpServletRequest p_request,
+            HttpServletResponse p_response, ServletContext p_context) throws ServletException,
+            IOException, EnvoyServletException
     {
         HttpSession session = p_request.getSession(false);
         String action = p_request.getParameter("action");
@@ -80,7 +82,8 @@ public class XmlRuleFileMainHandler
             }
             else if (XmlRuleConstant.NEW.equals(action))
             {
-                if (FormUtil.isNotDuplicateSubmisson(p_request, FormUtil.Forms.NEW_XML_RULE)) {
+                if (FormUtil.isNotDuplicateSubmisson(p_request, FormUtil.Forms.NEW_XML_RULE))
+                {
                     createRule(p_request, session);
                     clearSessionExceptTableInfo(session, XmlRuleConstant.XMLRULE_KEY);
                 }
@@ -92,12 +95,11 @@ public class XmlRuleFileMainHandler
             }
             else if (XmlRuleConstant.REMOVE.equals(action))
             {
-            	if (p_request.getMethod().equalsIgnoreCase(REQUEST_METHOD_GET)) 
-        		{
-        			p_response
-        					.sendRedirect("/globalsight/ControlServlet?activityName=xmlrules");
-        			return;
-        		}
+                if (p_request.getMethod().equalsIgnoreCase(REQUEST_METHOD_GET))
+                {
+                    p_response.sendRedirect("/globalsight/ControlServlet?activityName=xmlrules");
+                    return;
+                }
                 removeRule(p_request, session);
                 clearSessionExceptTableInfo(session, XmlRuleConstant.XMLRULE_KEY);
             }
@@ -121,46 +123,46 @@ public class XmlRuleFileMainHandler
     }
 
     private void removeRule(HttpServletRequest p_request, HttpSession session)
-			throws RemoteException, NamingException, GeneralException {
+            throws RemoteException, NamingException, GeneralException
+    {
 
-		String ids = (String) p_request.getParameter(RADIO_BUTTON);
-        String[] idarr=ids.split(" ");
-        for(int j=0;j<idarr.length;j++){
-        	String id=idarr[j];
-		// check whether some file profiles using it.
-		String companyId = CompanyThreadLocal.getInstance().getValue();
-		List xmlRuleFilters = FilterHelper.getXmlRuleFilters(id, companyId);
-		if(xmlRuleFilters.size() > 0)
-		{
-		    StringBuffer names = new StringBuffer();
-		    for (int i = 0; i< xmlRuleFilters.size(); i++)
-		    {
-		        XMLRuleFilter filter = (XMLRuleFilter) xmlRuleFilters.get(i);
-                if (i > 0)
+        String ids = (String) p_request.getParameter(RADIO_BUTTON);
+        String[] idarr = ids.split(" ");
+        for (int j = 0; j < idarr.length; j++)
+        {
+            String id = idarr[j];
+            // check whether some file profiles using it.
+            String companyId = CompanyThreadLocal.getInstance().getValue();
+            List xmlRuleFilters = FilterHelper.getXmlRuleFilters(id, companyId);
+            if (xmlRuleFilters.size() > 0)
+            {
+                StringBuffer names = new StringBuffer();
+                for (int i = 0; i < xmlRuleFilters.size(); i++)
                 {
-                    names.append(", ");
+                    XMLRuleFilter filter = (XMLRuleFilter) xmlRuleFilters.get(i);
+                    if (i > 0)
+                    {
+                        names.append(", ");
+                    }
+                    names.append(EditUtil.encodeXmlEntities(filter.getFilterName()));
                 }
-		        names.append(EditUtil.encodeXmlEntities(filter.getFilterName()));
-		    }
-            p_request.setAttribute("invalid",
-                    "This XML Rule is being used by the Xml Rule Filter: "
-                            + names);
-		}
-		else
-		{   
-			XmlRuleFile xrf = ServerProxy.getXmlRuleFilePersistenceManager()
-					.readXmlRuleFile(Long.parseLong(id));
-			ServerProxy.getXmlRuleFilePersistenceManager().deleteXmlRuleFile(
-					xrf);
-		}
+                p_request.setAttribute("invalid",
+                        "This XML Rule is being used by the Xml Rule Filter: " + names);
+            }
+            else
+            {
+                XmlRuleFile xrf = ServerProxy.getXmlRuleFilePersistenceManager().readXmlRuleFile(
+                        Long.parseLong(id));
+                ServerProxy.getXmlRuleFilePersistenceManager().deleteXmlRuleFile(xrf);
+            }
         }
-	}
+    }
 
-	/**
-	 * Create a new rule.
-	 */
+    /**
+     * Create a new rule.
+     */
     private void createRule(HttpServletRequest p_request, HttpSession p_session)
-        throws RemoteException, NamingException, GeneralException
+            throws RemoteException, NamingException, GeneralException
     {
         XmlRuleFileImpl ruleFile = new XmlRuleFileImpl();
 
@@ -173,12 +175,12 @@ public class XmlRuleFileMainHandler
      * Modify an existing rule.
      */
     private void updateRule(HttpServletRequest p_request, HttpSession p_session)
-        throws RemoteException, NamingException, GeneralException
+            throws RemoteException, NamingException, GeneralException
     {
-        SessionManager sessionMgr = (SessionManager)p_session.getAttribute(
-            WebAppConstants.SESSION_MANAGER);
-        XmlRuleFileImpl ruleFile = (XmlRuleFileImpl)sessionMgr.getAttribute(
-            XmlRuleConstant.XMLRULE_KEY);
+        SessionManager sessionMgr = (SessionManager) p_session
+                .getAttribute(WebAppConstants.SESSION_MANAGER);
+        XmlRuleFileImpl ruleFile = (XmlRuleFileImpl) sessionMgr
+                .getAttribute(XmlRuleConstant.XMLRULE_KEY);
 
         getParams(p_request, ruleFile);
 
@@ -203,124 +205,115 @@ public class XmlRuleFileMainHandler
      * Get list of all rules.
      */
     private void dataForTable(HttpServletRequest p_request, HttpSession p_session)
-        throws RemoteException, NamingException, GeneralException
+            throws RemoteException, NamingException, GeneralException
     {
-        Collection xmlrulefiles = ServerProxy.getXmlRuleFilePersistenceManager().
-            getAllXmlRuleFiles();
-        Locale uiLocale = (Locale)p_session.getAttribute(WebAppConstants.UILOCALE);
-        
+        Collection xmlrulefiles = ServerProxy.getXmlRuleFilePersistenceManager()
+                .getAllXmlRuleFiles();
+        Locale uiLocale = (Locale) p_session.getAttribute(WebAppConstants.UILOCALE);
+
         SessionManager sessionManager = (SessionManager) p_session
                 .getAttribute(WebAppConstants.SESSION_MANAGER);
-        String xmlruleName = (String) sessionManager
-                .getAttribute("xmlruleName");
-        String xmlruleCompName = (String) sessionManager
-        		.getAttribute("xmlruleCompName"); 
-        
-        
-        Object[] extensions=xmlrulefiles.toArray();
-        ArrayList fes=new ArrayList();
+        String xmlruleName = (String) sessionManager.getAttribute("xmlruleName");
+        String xmlruleCompName = (String) sessionManager.getAttribute("xmlruleCompName");
 
+        Object[] extensions = xmlrulefiles.toArray();
+        ArrayList fes = new ArrayList();
 
-        
-        if(xmlruleName!=""||xmlruleCompName!="")
+        if (xmlruleName != "" || xmlruleCompName != "")
         {
-        if(xmlruleName!=null && xmlruleName!="")
-        {
-         for(int i=0;i<extensions.length;i++)
-         {
-        	if(extensions[i].toString().toLowerCase().indexOf(xmlruleName.toLowerCase())>=0)
-        	 {      	
-        		fes.add(extensions[i]);
-        	 }
-         }
-        }
-        
-        else if(xmlruleCompName!=null && xmlruleCompName!=""){
-        	for(int i=0;i<extensions.length;i++)
+            if (xmlruleName != null && xmlruleName != "")
             {
-               String compName = CompanyWrapper.getCompanyNameById(
-                		((XmlRuleFileImpl) extensions[i]).getCompanyId()).toLowerCase();
-           	if(compName.toLowerCase().indexOf(xmlruleCompName.toLowerCase())>=0)
-           	 {      	
-           		fes.add(extensions[i]);
-           	 }
+                for (int i = 0; i < extensions.length; i++)
+                {
+                    if (extensions[i].toString().toLowerCase().indexOf(xmlruleName.toLowerCase()) >= 0)
+                    {
+                        fes.add(extensions[i]);
+                    }
+                }
             }
-        }
-        else{
-        	
-         for(int i=0;i<extensions.length;i++)
-             {     		
-       		    fes.add(extensions[i]);        	
-             }
-         
-        }
+
+            else if (xmlruleCompName != null && xmlruleCompName != "")
+            {
+                for (int i = 0; i < extensions.length; i++)
+                {
+                    String compName = CompanyWrapper.getCompanyNameById(
+                            ((XmlRuleFileImpl) extensions[i]).getCompanyId()).toLowerCase();
+                    if (compName.toLowerCase().indexOf(xmlruleCompName.toLowerCase()) >= 0)
+                    {
+                        fes.add(extensions[i]);
+                    }
+                }
+            }
+            else
+            {
+
+                for (int i = 0; i < extensions.length; i++)
+                {
+                    fes.add(extensions[i]);
+                }
+
+            }
         }
         else
         {
-            for(int i=0;i<extensions.length;i++)
-            {     		
-      		    fes.add(extensions[i]);        	
-            }	
+            for (int i = 0; i < extensions.length; i++)
+            {
+                fes.add(extensions[i]);
+            }
         }
-        
 
-        
-        int numPerPage=getNumPerPage(p_request,p_session);
-        setTableNavigation(p_request, p_session,fes,
-            new XmlRuleFileComparator(uiLocale),
-            numPerPage,
-            XmlRuleConstant.XMLRULE_LIST, XmlRuleConstant.XMLRULE_KEY);
+        int numPerPage = getNumPerPage(p_request, p_session);
+        setTableNavigation(p_request, p_session, fes, new XmlRuleFileComparator(uiLocale),
+                numPerPage, XmlRuleConstant.XMLRULE_LIST, XmlRuleConstant.XMLRULE_KEY);
     }
 
-private int getNumPerPage(HttpServletRequest p_request,
-		HttpSession p_session)
-{
-	int result = 10;
-
-	SessionManager sessionManager = (SessionManager) p_session
-			.getAttribute(WebAppConstants.SESSION_MANAGER);
-	String xmlruleNumPerPage = p_request.getParameter("numOfPageSize");
-	if (StringUtil.isEmpty(xmlruleNumPerPage))
-	{
-		xmlruleNumPerPage = (String) sessionManager.getAttribute("xmlruleNumPerPage");
-	}
-
-	if (xmlruleNumPerPage != null)
-	{
-		sessionManager.setAttribute("xmlruleNumPerPage", xmlruleNumPerPage.trim());
-		if ("all".equalsIgnoreCase(xmlruleNumPerPage))
-		{
-			result = Integer.MAX_VALUE;
-		}
-		else
-		{
-			try
-			{
-				result = Integer.parseInt(xmlruleNumPerPage);
-			}
-			catch (NumberFormatException ignore)
-			{
-				result = 10;
-			}
-		}
-	}
-
-	return result;
-}
-
-private void handleFilters(HttpServletRequest p_request,
-        SessionManager sessionMgr, String action)
-{
-    String xmlruleName = (String) p_request.getParameter("xmlruleName");
-    String xmlruleCompName=(String)p_request.getParameter("xmlruleCompName");
-    if (p_request.getMethod().equalsIgnoreCase(
-            WebAppConstants.REQUEST_METHOD_GET))
+    private int getNumPerPage(HttpServletRequest p_request, HttpSession p_session)
     {
-    	xmlruleName = (String) sessionMgr.getAttribute("xmlruleName");
-    	xmlruleCompName=(String)sessionMgr.getAttribute("xmlruleCompName");
+        int result = 10;
+
+        SessionManager sessionManager = (SessionManager) p_session
+                .getAttribute(WebAppConstants.SESSION_MANAGER);
+        String xmlruleNumPerPage = p_request.getParameter("numOfPageSize");
+        if (StringUtil.isEmpty(xmlruleNumPerPage))
+        {
+            xmlruleNumPerPage = (String) sessionManager.getAttribute("xmlruleNumPerPage");
+        }
+
+        if (xmlruleNumPerPage != null)
+        {
+            sessionManager.setAttribute("xmlruleNumPerPage", xmlruleNumPerPage.trim());
+            if ("all".equalsIgnoreCase(xmlruleNumPerPage))
+            {
+                result = Integer.MAX_VALUE;
+            }
+            else
+            {
+                try
+                {
+                    result = Integer.parseInt(xmlruleNumPerPage);
+                }
+                catch (NumberFormatException ignore)
+                {
+                    result = 10;
+                }
+            }
+        }
+
+        return result;
     }
-    sessionMgr.setAttribute("xmlruleName", xmlruleName);
-    sessionMgr.setAttribute("xmlruleCompName",xmlruleCompName);
-}
+
+    private void handleFilters(HttpServletRequest p_request, SessionManager sessionMgr,
+            String action)
+    {
+        String xmlruleName = (String) p_request.getParameter("xmlruleName");
+        String xmlruleCompName = (String) p_request.getParameter("xmlruleCompName");
+        if (p_request.getMethod().equalsIgnoreCase(WebAppConstants.REQUEST_METHOD_GET))
+        {
+            xmlruleName = (String) sessionMgr.getAttribute("xmlruleName");
+            xmlruleCompName = (String) sessionMgr.getAttribute("xmlruleCompName");
+        }
+        sessionMgr.setAttribute("xmlruleName", xmlruleName);
+        sessionMgr.setAttribute("xmlruleCompName", xmlruleCompName);
+    }
 
 }

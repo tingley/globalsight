1b45b323d635556f24dae502d75457e342180caa
allie.su <allie.su@welocalize.com>
2016-04-29 17:49:06 +0800

    0001673: Can't export json file.

---
+++ main6/ling/com/globalsight/ling/common/JsonEnDecoder.java
@@ -0,0 +1,60 @@
+/**
+ *  Copyright 2009 Welocalize, Inc. 
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  
+ *  You may obtain a copy of the License at 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+package com.globalsight.ling.common;
+
+public class JsonEnDecoder
+    extends NativeEnDecoder
+{
+
+    @Override
+    public String decode(String p_nativeString) throws NativeEnDecoderException
+    {
+        return null;
+    }
+
+    @Override
+    public String decode(String p_nativeString, String p_outerQuote)
+            throws NativeEnDecoderException
+    {
+        return null;
+    }
+
+    @Override
+    public String encode(String p_nativeString) throws NativeEnDecoderException
+    {
+        return null;
+    }
+
+    @Override
+    public String encode(String p_nativeString, String p_outerQuote)
+            throws NativeEnDecoderException
+    {
+        return null;
+    }
+
+    @Override
+    public String encodeWithEncodingCheck(String p_nativeString) throws NativeEnDecoderException
+    {
+        return null;
+    }
+
+    @Override
+    public String encodeWithEncodingCheck(String p_nativeString, String p_outerQuote)
+            throws NativeEnDecoderException
+    {
+        return null;
+    }}

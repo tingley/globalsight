f26e7531776cf3d351310b407e40fb214a228b16
YorkJin <YorkJin@YorkJin-Win7-PC.welocalize.com>
2016-05-10 12:25:59 +0800

    GBS-4364: poor import performance between 8.6.2 and later

--- main6/ling/com/globalsight/ling/tm3/core/Fingerprint.java
+++ main6/ling/com/globalsight/ling/tm3/core/Fingerprint.java
@@ -6,30 +6,41 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * Compute fingerprints for database matching.  This provides 
- * a default implementation of fingerprint calculation that takes
- * the low 8 bytes of an MD5 hash of the input.  Other schemes
- * are possible.
+ * Compute fingerprints for database matching. This provides a default
+ * implementation of fingerprint calculation that takes the low 8 bytes of an
+ * MD5 hash of the input. Other schemes are possible.
  */
-public class Fingerprint {
+public class Fingerprint
+{
+    static MessageDigest digest = null;
 
-    public static long fromString(String data) { 
+    public static long fromString(String data)
+    {
         return getMD5asLong(data);
     }
-    
+
     private static final Charset UTF8 = Charset.forName("UTF-8");
-    private static byte[] getMD5(String s) {
-        MessageDigest digest;
-        try {
-            digest = java.security.MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e); // can't happen
+
+    private static byte[] getMD5(String s)
+    {
+        if (digest == null)
+        {
+            try
+            {
+                digest = java.security.MessageDigest.getInstance("MD5");
+            }
+            catch (NoSuchAlgorithmException e)
+            {
+                throw new RuntimeException(e); // can't happen
+            }
         }
+
         digest.update(s.getBytes(UTF8));
         return digest.digest();
     }
 
-    private static long getMD5asLong(String s) {
+    private static long getMD5asLong(String s)
+    {
         byte[] bytes = getMD5(s);
         ByteBuffer buf = ByteBuffer.allocate(8);
         buf.put(bytes, 0, 8);

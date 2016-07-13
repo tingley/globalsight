cb5439a5fc03ff1538f457f40861eb24027336f2
YorkJin <YorkJin@YorkJin-Win7-PC.welocalize.com>
2016-07-13 15:32:20 +0800

    Move "main6/QA_Automation" files to "Internal_Tools" repository.

--- main6/QA_Automation/Selenium/src/com/globalsight/selenium/pages/Login.java
+++
@@ -1,8 +0,0 @@
-package com.globalsight.selenium.pages;
-
-public class Login
-{
-    public static final String LOGIN_NAME_TEXT = "nameField";
-    public static final String LOGIN_PASSWORD_TEXT = "passwordField";
-    public static final String LOGIN_SUBMIT_BUTTON = "login0";
-}

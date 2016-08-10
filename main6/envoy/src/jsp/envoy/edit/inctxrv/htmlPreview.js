2e60a5b0c1ead5a66740ad31551b13405b4f98a5
malcolm.liu <malcolm.liu>
2016-08-10 20:54:19 +0800

    can not show well in list view

--- main6/envoy/src/jsp/envoy/edit/inctxrv/htmlPreview.js
+++ main6/envoy/src/jsp/envoy/edit/inctxrv/htmlPreview.js
@@ -2,42 +2,10 @@
 function HighlightHtmlPreviewSegment(tuId, tuvId, subId)
 {
     var o = getSegment(tuId, tuvId, subId);
-
-    if (o_currentSegment != null)
-    {
-    	unhighlightHtmlPreviewSegment(o_currentSegment);
-    }
-
     if (o != null)
     {
-    	highlightHtmlPreviewSegment(o);
-    }
-
-    o_currentSegment = o;
-}
-
-function highlightHtmlPreviewSegment(o)
-{
-    if (o != null)
-    {
-        o.className = "highlight";
-        o.scrollIntoView(true);
-    }
-}
-
-function UnhighlightHtmlPreviewSegment(tuId, tuvId, subId)
-{
-    var o = getSegment(tuId, tuvId, subId);
-
-    unhighlightHtmlPreviewSegment(o);
-
-    o_currentSegment = o;
-}
-
-function unhighlightHtmlPreviewSegment(o)
-{
-    if (o != null)
-    {
-        o.className = "";
+       $(".gsHighlight").removeClass("gsHighlight");
+       $(o).addClass("gsHighlight");
+       o.scrollIntoView(true);
     }
 }

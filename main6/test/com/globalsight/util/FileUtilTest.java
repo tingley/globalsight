09410878b456132070d17a29a1cf3c476a8d035f
Leo Chen <leo.chen@welocalize.com>
2016-08-25 16:37:39 +0800

    GBS-4504 unit test

--- main6/test/com/globalsight/util/FileUtilTest.java
+++ main6/test/com/globalsight/util/FileUtilTest.java
@@ -16,16 +16,21 @@
  */
 package com.globalsight.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
-import org.junit.*;
 
-public class FileUtilTest {
+import org.junit.Test;
+
+public class FileUtilTest
+{
 
     @Test
-    public void testXmlFileEncoding() throws Exception {
+    public void testXmlFileEncoding() throws Exception
+    {
         // Assume UTF-8 if we can't find a BOM or a header
         assertEquals("UTF-8", getEncoding("testdata/utf8_default.xml"));
         // Read the BOM
@@ -34,13 +39,21 @@ public class FileUtilTest {
         assertEquals("UTF-16", getEncoding("testdata/utf16.xml"));
     }
 
-    protected String getEncoding(String filename) throws IOException {
+    protected String getEncoding(String filename) throws IOException
+    {
         return FileUtil.getEncodingOfXml(getTestFile(filename));
     }
-    
-    public File getTestFile(String filename) throws IOException {
+
+    public File getTestFile(String filename) throws IOException
+    {
         String fullname = getClass().getResource(filename).getFile();
         return new File(fullname);
+    }
+
+    public File getTestFile(Class className, String filename) throws IOException
+    {
+        String filePath = className.getResource(filename).getFile();
+        return new File(filePath);
     }
 
     @Test
@@ -75,30 +88,36 @@ public class FileUtilTest {
         assertTrue(child.delete());
         assertTrue(tmpFile.delete());
     }
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         org.junit.runner.JUnitCore.main("com.globalsight.util.FileUtilTest");
     }
-    
+
     @Test
-    public void testIsUTFFormat() {
+    public void testIsUTFFormat()
+    {
         assertTrue(FileUtil.isUTFFormat("UTF-8"));
         assertTrue(FileUtil.isUTFFormat("UTF-16LE"));
         assertTrue(FileUtil.isUTFFormat("UTF-16BE"));
         assertTrue(FileUtil.isUTFFormat("UTF-16"));
         assertFalse(FileUtil.isUTFFormat("GBK"));
     }
-    
+
     @Test
-    public void testGuessEncoding() throws IOException {
+    public void testGuessEncoding() throws IOException
+    {
         assertEquals(null, FileUtil.guessEncoding(getTestFile("testdata/utf8_default.xml")));
         assertEquals(FileUtil.UTF8, FileUtil.guessEncoding(getTestFile("testdata/demo_utf8.html")));
-        assertEquals(FileUtil.UTF16LE, FileUtil.guessEncoding(getTestFile("testdata/demo_utf16.html")));
-        assertEquals(FileUtil.UTF16BE, FileUtil.guessEncoding(getTestFile("testdata/demo_utf16BE.html")));
+        assertEquals(FileUtil.UTF16LE,
+                FileUtil.guessEncoding(getTestFile("testdata/demo_utf16.html")));
+        assertEquals(FileUtil.UTF16BE,
+                FileUtil.guessEncoding(getTestFile("testdata/demo_utf16BE.html")));
     }
-    
+
     @Test
-    public void testIsNeedBOMProcessing() {
+    public void testIsNeedBOMProcessing()
+    {
         assertTrue(FileUtil.isNeedBOMProcessing("test.html"));
         assertTrue(FileUtil.isNeedBOMProcessing("test.htm"));
         assertTrue(FileUtil.isNeedBOMProcessing("test.HTML"));
@@ -107,7 +126,8 @@ public class FileUtilTest {
     }
 
     @Test
-    public void testReadWriteFile() throws Exception {
+    public void testReadWriteFile() throws Exception
+    {
         File tmpFile;
 
         tmpFile = File.createTempFile("GSTestReadWrite", null);
@@ -116,14 +136,16 @@ public class FileUtilTest {
         FileUtil.deleteTempFile(tmpFile);
 
         // Not easy to test the atomic part
-        tmpFile = new File(System.getProperty("java.io.tmpdir") + File.separator + "GSTestReadWrite.tmp");
+        tmpFile = new File(
+                System.getProperty("java.io.tmpdir") + File.separator + "GSTestReadWrite.tmp");
         FileUtil.writeFileAtomically(tmpFile, "hello world", "US-ASCII");
         assertEquals("hello world", FileUtil.readFile(tmpFile, "US-ASCII"));
         FileUtil.deleteTempFile(tmpFile);
     }
-    
+
     @Test
-    public void testIsWindowsReturnMethod() throws Exception {
+    public void testIsWindowsReturnMethod() throws Exception
+    {
         String filename = "";
         filename = getTestFile("testdata/test_w.resx").getAbsolutePath();
         assertTrue(FileUtil.isWindowsReturnMethod(filename));

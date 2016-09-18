c81e682612d5a5730ea73f78d93559543be72be1
malcolm.liu <malcolm.liu>
2016-09-18 13:51:50 +0800

    for GBS-4401, preview

--- main6/envoy/src/java/com/globalsight/everest/webapp/pagehandler/edit/online/EditorPageHandler.java
+++ main6/envoy/src/java/com/globalsight/everest/webapp/pagehandler/edit/online/EditorPageHandler.java
@@ -943,7 +943,11 @@ public class EditorPageHandler extends PageHandler implements EditorConstants
         {
             if (layout.getSourceViewMode() == VIEWMODE_PREVIEW)
             {
-                layout.setSourceViewMode(DEFAULT_VIEWMODE_IF_NO_PREVIEW);
+            	// for GBS-4401, preview
+                if (p_state.getSourcePreviewFile() == null)
+                {
+                    layout.setSourceViewMode(DEFAULT_VIEWMODE_IF_NO_PREVIEW);
+                }
             }
 
             if (layout.getTargetViewMode() == VIEWMODE_PREVIEW)

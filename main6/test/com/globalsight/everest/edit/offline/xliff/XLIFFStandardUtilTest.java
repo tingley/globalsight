dca2c7b5f1b16acb5768f5a5c5998de2fe4d94b9
malcolm.liu <malcolm.liu>
2016-10-10 15:00:19 +0800

    GBS-4432 xliff ctype not compliant

--- main6/test/com/globalsight/everest/edit/offline/xliff/XLIFFStandardUtilTest.java
+++ main6/test/com/globalsight/everest/edit/offline/xliff/XLIFFStandardUtilTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.globalsight.everest.edit.offline.page.OfflineSegmentData;
+
 public class XLIFFStandardUtilTest
 {
 
@@ -43,4 +45,31 @@ public class XLIFFStandardUtilTest
                 "119462");
         Assert.assertEquals(expected, segment);
     }
+    
+    /**
+     * For GBS-4432 xliff ctype not compliant
+     */
+    @Test
+    public final void testConvertToStandard2()
+    {
+        String segment = "18-29 July<ph i=\"1\" type=\"tag\" x=\"1\">&lt;wr&gt;&lt;w:r&gt;&lt;w:rPr&gt;&lt;w:rFonts w:ascii=\"Gill Sans MT\" w:cs=\"Gill Sans Light\" w:hAnsi=\"Gill Sans MT\"&gt;&lt;/w:rFonts&gt;&lt;w:color w:themeColor=\"text2\" w:themeShade=\"80\" w:val=\"0F243E\"&gt;&lt;/w:color&gt;&lt;w:sz w:val=\"26\"&gt;&lt;/w:sz&gt;&lt;w:szCs w:val=\"26\"&gt;&lt;/w:szCs&gt;&lt;/w:rPr&gt;&lt;w:tab&gt;&lt;/w:tab&gt;&lt;/w:r&gt;&lt;/wr&gt;</ph>";
+        String expected = "18-29 July<ph id=\"-1_GS_1\" i=\"1\" ctype=\"x-tag\" xid=\"1\">&lt;wr&gt;&lt;w:r&gt;&lt;w:rPr&gt;&lt;w:rFonts w:ascii=\"Gill Sans MT\" w:cs=\"Gill Sans Light\" w:hAnsi=\"Gill Sans MT\"&gt;&lt;/w:rFonts&gt;&lt;w:color w:themeColor=\"text2\" w:themeShade=\"80\" w:val=\"0F243E\"&gt;&lt;/w:color&gt;&lt;w:sz w:val=\"26\"&gt;&lt;/w:sz&gt;&lt;w:szCs w:val=\"26\"&gt;&lt;/w:szCs&gt;&lt;/w:rPr&gt;&lt;w:tab&gt;&lt;/w:tab&gt;&lt;/w:r&gt;&lt;/wr&gt;</ph>";
+        OfflineSegmentData osd = new OfflineSegmentData("-1");
+        segment = XLIFFStandardUtil.convertToStandard(osd, segment);
+       
+        Assert.assertEquals(expected, segment);
+    }
+    
+    /**
+     * For GBS-4432 xliff ctype not compliant
+     */
+    @Test
+    public final void testConvertToTmx()
+    {
+        String segment = "18-29 July<ph i=\"1\" type=\"tag\" x=\"1\">&lt;wr&gt;&lt;w:r&gt;&lt;w:rPr&gt;&lt;w:rFonts w:ascii=\"Gill Sans MT\" w:cs=\"Gill Sans Light\" w:hAnsi=\"Gill Sans MT\"&gt;&lt;/w:rFonts&gt;&lt;w:color w:themeColor=\"text2\" w:themeShade=\"80\" w:val=\"0F243E\"&gt;&lt;/w:color&gt;&lt;w:sz w:val=\"26\"&gt;&lt;/w:sz&gt;&lt;w:szCs w:val=\"26\"&gt;&lt;/w:szCs&gt;&lt;/w:rPr&gt;&lt;w:tab&gt;&lt;/w:tab&gt;&lt;/w:r&gt;&lt;/wr&gt;</ph>";
+        String expected = "18-29 July<ph i=\"1\" type=\"tag\" x=\"1\">&lt;wr&gt;&lt;w:r&gt;&lt;w:rPr&gt;&lt;w:rFonts w:ascii=\"Gill Sans MT\" w:cs=\"Gill Sans Light\" w:hAnsi=\"Gill Sans MT\"&gt;&lt;/w:rFonts&gt;&lt;w:color w:themeColor=\"text2\" w:themeShade=\"80\" w:val=\"0F243E\"&gt;&lt;/w:color&gt;&lt;w:sz w:val=\"26\"&gt;&lt;/w:sz&gt;&lt;w:szCs w:val=\"26\"&gt;&lt;/w:szCs&gt;&lt;/w:rPr&gt;&lt;w:tab&gt;&lt;/w:tab&gt;&lt;/w:r&gt;&lt;/wr&gt;</ph>";
+        segment = XLIFFStandardUtil.convertToTmx(segment);
+        Assert.assertEquals(expected, segment);
+    }
+    
 }

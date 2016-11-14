727da1d3b6c00395f501f53f0460a2acd67929b5
YorkJin <YorkJin@YorkJin-Win7-PC.welocalize.com>
2016-11-14 14:34:06 +0800

    0002063: GBS-4415: Cannot run MergeProperties directly.

--- main6/tools/install/MergeProperties.bat
+++ main6/tools/install/MergeProperties.bat
@@ -3,4 +3,4 @@ rem Top level MergeProperties script for NT
 rem java 1.4 must be in the path in order to run
 rem USAGE: MergeProperties [-noUI] [-previousHome <path to previous Ambassador home>]
 
-java -classpath .;installer.jar MergePropertiesGUI %*
+java -classpath .;installer.jar;lib/commons-codec-1.3.jar;lib/ant.jar;lib/log4j.jar MergePropertiesGUI %*
